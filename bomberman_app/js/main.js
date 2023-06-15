71bfaaacd345ba3a6d47d66773298207a0b30a3c
Malin <malin.oscarius@gritlab.ax>
2023-06-15 16:18:03 +0300

    placement of chat and map

--- bomberman_app/js/main.js
+++ bomberman_app/js/main.js
@@ -1,6 +1,11 @@
 import MiniFramework from "../../mini_framework/mini-framework.js";
 import createMap from "./game_map.js";
 
+let playername = "";
+let playernames = [];
+let nickNameAdded = false;
+let gameStarted = false;
+
 const Start = () => {
     return `
     <MF>
@@ -8,26 +13,48 @@ const Start = () => {
     <div class="core-part">
       <div id="game" class="game">
         <div class="howtoplay" style="text-align: center;">Use arrows to move, shift to place bombs</div>
-        <div class="stats" style="height: 45px; width: 1125px; top: 130px;">
+        <div class="stats" style="height: 45px; width: 900px; top: 130px;">
           <div class="lives">Lives: 3</div>
           <div class="timer">Time: 3:00</div>
           <div class="score">Score: 0</div>
         </div>
-        <div class="naming" style="background: url(&quot;bomberman_app/img/story.png&quot;); height: 900px;">
-          <div class="textfield" style="align-self: center;">Type in your nickname, then press ENTER</div>
-          <input class="playername" id="nameplayer" maxlength="15" placeholder="add nickname here...">
-          <div class="invalidnotice" style="align-self: center;">Only letters and numbers allowed</div>
-        </div>
+      </div>
+      <div class="player-chat">
+        <h2>CHAT</h2>
+        <p>Wait for other players...</p>
       </div>
     </div>
     </MF>
     `;
 }
 
-const container = document.getElementById("root");
+const EnterName = () => {
+    return `
+    <MF>
+        <div class="naming" style="background: url(&quot;bomberman_app/img/story.png&quot;); height: 900px width: 900px;">
+          <div class="textfield" style="align-self: center;">Type in your nickname, then press ENTER</div>
+          <input class="playername" id="nameplayer" maxlength="15" placeholder="add nickname here...">
+          <div class="invalidnotice" style="align-self: center;">Only letters and numbers allowed</div>
+        </div>
+    </MF>
+    `;
+}
+
+const WaitingView = () => {
+    return `
+    <MF>
+    <div class="await-players">Waiting for other Players</div>
+    </MF>
+    `;
+}
+
+
+let container = document.getElementById("root");
 container.innerHTML = "";
 MiniFramework.render(Start, container);
+
 createMap();
+
 
 // Set focus on the input textfield when the page is loaded
 window.onload = () => {
