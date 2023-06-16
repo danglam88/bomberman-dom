1688dab6082b29258772bf76964cb8225cb6486e
Danilo Costa Canguçu <97832010+danilocangucu@users.noreply.github.com>
2023-06-16 18:51:25 +0300

    chat feature

--- bomberman_app/frontend/js/main.js
+++ bomberman_app/frontend/js/main.js
@@ -13,21 +13,10 @@ export const Title = () => {
     <h1>BOMBERMAN • DOM</h1>
   </MF>
   `;
-}
-
-export const Chat = () => {
-  return `
-  <MF>
-    <div class="player-chat">
-      <h2>CHAT</h2>
-      <p>Wait for other players...</p>
-    </div>
-  </MF>
-  `;
-}
+};
 
 export const Info = () => {
-    return `
+  return `
     <MF>
       <div class="howtoplay" style="text-align: center;">Use arrows to move, shift to place bombs</div>
       <div class="stats" style="height: 45px; width: 900px; top: 130px;">
@@ -47,32 +36,34 @@ export const Naming = () => {
       let options = {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ "nickname": event.target.value })
+        body: JSON.stringify({ nickname: event.target.value }),
       };
 
       fetch("/validate", options)
-        .then(response => {
+        .then((response) => {
           if (response.status === 200) {
             localStorage.setItem("nickname", event.target.value);
             window.location.hash = "#/waiting";
-            openChat()
+            setTimeout(openChat, 100);
           } else if (response.status === 409) {
-            validateError = "Nickname was already taken, please choose another one";
+            validateError =
+              "Nickname was already taken, please choose another one";
             MiniFramework.updateState();
           } else if (response.status === 429) {
-            validateError = "There are already 4 players in the game, please try again later";
+            validateError =
+              "There are already 4 players in the game, please try again later";
             MiniFramework.updateState();
           }
         })
-        .catch(error => {
-          console.error(error)
-        })
+        .catch((error) => {
+          console.error(error);
+        });
     }
-  }
+  };
 
-  MiniFramework.defineFunc(validateInput)
+  MiniFramework.defineFunc(validateInput);
 
   return `
   <MF>
@@ -80,74 +71,112 @@ export const Naming = () => {
       <div class="textfield" style="align-self: center;">Type in your nickname, then press ENTER</div>
       <input class="playername" id="nameplayer" maxlength="15" placeholder="add nickname here..." onkeypress="validateInput">
       <div class="invalidnotice" style="align-self: center;">Only letters and numbers are allowed</div>
-      ${validateError !== "" ? `<div class="invalidnotice" style="align-self: center;">${validateError}</div>` : ""}
+      ${
+        validateError !== ""
+          ? `<div class="invalidnotice" style="align-self: center;">${validateError}</div>`
+          : ""
+      }
     </div>
   </MF>
   `;
-}
+};
 
 export const Counter = () => {
   if (!playersFetched) {
-  fetch("/players")
-    .then(response => response.json())
-    .then(data => {
-      playersFetched = true;
-      players = data;
+    fetch("/players")
+      .then((response) => response.json())
+      .then((data) => {
+        playersFetched = true;
+        players = data;
 
-      if (data.length > 1 && data.length <= 4) {
-        timer = 10;
-      }
+        if (data.length > 1 && data.length <= 4) {
+          timer = 10;
+        }
 
-      if (data.length > 1 && data.length < 4) {
-        waitTime = 20;
-      }
+        if (data.length > 1 && data.length < 4) {
+          waitTime = 20;
+        }
 
-      MiniFramework.updateState();
+        MiniFramework.updateState();
 
-      if (waitTime !== undefined) {
-        const waitTimeId = setInterval(() => {
-          waitTime--;
-          MiniFramework.updateState();
+        if (waitTime !== undefined) {
+          const waitTimeId = setInterval(() => {
+            waitTime--;
+            MiniFramework.updateState();
 
-          if (waitTime === 0) {
-            clearInterval(waitTimeId);
+            if (waitTime === 0) {
+              clearInterval(waitTimeId);
 
-            if (timer !== undefined) {
-              const timerId = setInterval(() => {
-                timer--;
-                MiniFramework.updateState();
+              if (timer !== undefined) {
+                const timerId = setInterval(() => {
+                  timer--;
+                  MiniFramework.updateState();
 
-                if (timer === 0) {
-                  clearInterval(timerId);
-                  window.location.hash = "#/gamestart";
-                }
-              }, 1000);
+                  if (timer === 0) {
+                    clearInterval(timerId);
+                    window.location.hash = "#/gamestart";
+                  }
+                }, 1000);
+              }
             }
-          }
-        }, 1000);
-      }
-    })
-    .catch(error => {
-      console.error(error)
-    })
+          }, 1000);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   return `
   <MF>
     <div class="start" style="background: url(&quot;img/story.png&quot;); height: 900px; width: 900px;">
       <div class="storytext" style="align-self: center;">
-        ${localStorage.getItem("nickname").trim().length > 0 && Array.isArray(players) && players.length === 1
-          ? `You (${localStorage.getItem("nickname")}) are the only one who joined the game. Let's wait for other players...`
-          : localStorage.getItem("nickname").trim().length > 0 && Array.isArray(players) && players.length > 1 && players.length < 4 && waitTime !== undefined && waitTime > 0
-            ? `There are totally ${players.length} players in the game: You (${localStorage.getItem("nickname")}), ${players.filter(player => player.name !== localStorage.getItem("nickname")).map(player => player.name).join(", ")}. Let's wait for ${waitTime} more seconds...`
-            : localStorage.getItem("nickname").trim().length > 0 && Array.isArray(players) && players.length > 1 && players.length <= 4 && timer !== undefined && timer > 0
-              ? `There are totally ${players.length} players in the game: You (${localStorage.getItem("nickname")}), ${players.filter(player => player.name !== localStorage.getItem("nickname")).map(player => player.name).join(", ")}. Game will start in ${timer} seconds...`
-              : "Please type in your nickname first"}
+        ${
+          localStorage.getItem("nickname").trim().length > 0 &&
+          Array.isArray(players) &&
+          players.length === 1
+            ? `You (${localStorage.getItem(
+                "nickname"
+              )}) are the only one who joined the game. Let's wait for other players...`
+            : localStorage.getItem("nickname").trim().length > 0 &&
+              Array.isArray(players) &&
+              players.length > 1 &&
+              players.length < 4 &&
+              waitTime !== undefined &&
+              waitTime > 0
+            ? `There are totally ${
+                players.length
+              } players in the game: You (${localStorage.getItem(
+                "nickname"
+              )}), ${players
+                .filter(
+                  (player) => player.name !== localStorage.getItem("nickname")
+                )
+                .map((player) => player.name)
+                .join(", ")}. Let's wait for ${waitTime} more seconds...`
+            : localStorage.getItem("nickname").trim().length > 0 &&
+              Array.isArray(players) &&
+              players.length > 1 &&
+              players.length <= 4 &&
+              timer !== undefined &&
+              timer > 0
+            ? `There are totally ${
+                players.length
+              } players in the game: You (${localStorage.getItem(
+                "nickname"
+              )}), ${players
+                .filter(
+                  (player) => player.name !== localStorage.getItem("nickname")
+                )
+                .map((player) => player.name)
+                .join(", ")}. Game will start in ${timer} seconds...`
+            : "Please type in your nickname first"
+        }
       </div>
     </div>
   </MF>
   `;
-}
+};
 
 export const GameMap = () => {
   return `
@@ -300,123 +329,140 @@ export const GameMap = () => {
     </div>
   </MF>
   `;
-}
+};
 
 export const Start = () => {
   return `
   <MF>
     ${Title()}
-    <div class="core-part">
+    <div id="core-part" class="core-part">
       <div id="game" class="game">
         ${Info()}
         ${Naming()}
       </div>
-      ${Chat()}
-    </div>
-  </MF>
-  `;
-}
-
-export const Waiting = () => {
-  return `
-  <MF>
-    ${Title()}
-    <div class="core-part">
-      <div id="game" class="game">
-        ${Info()}
-        ${Counter()}
-      </div>
-      ${Chat()}
-    </div>
-  </MF>
-  `;
-}
-
-export const GameStart = () => {
-  return `
-  <MF>
-    ${Title()}
-    <div class="core-part">
-      <div id="game" class="game">
-        ${Info()}
-        ${GameMap()}
-      </div>
-      ${Chat()}
     </div>
   </MF>
   `;
 };
 
-  function Router() {
-    function routeChange() {
-      const container = document.getElementById("root");
-      container.innerHTML = "";
-      if (window.location.hash !== "#/waiting" && window.location.hash !== "#/gamestart" && window.location.hash !== "#/gameover") {
-        MiniFramework.render(Start, container);
-      } else if (window.location.hash === "#/waiting") {
-        MiniFramework.render(Waiting, container);
-      } else if (window.location.hash === "#/gamestart") {
-        MiniFramework.render(GameStart, container);
-      }
+export const Waiting = () => {
+  return `
+  <MF>
+    ${Title()}
+    <div id="core-part" class="core-part">
+      <div id="game" class="game">
+        ${Info()}
+        ${Counter()}
+      </div>
+    </div>
+  </MF>
+  `;
+};
 
-  // Set focus on the input textfield when the page is loaded
-  window.onload = () => {
-    const input = document.getElementById("nameplayer");
-    if (input !== null) {
-      input.focus();
+export const GameStart = () => {
+  return `
+  <MF>
+    ${Title()}
+    <div id="core-part" class="core-part">
+      <div id="game" class="game">
+        ${Info()}
+        ${GameMap()}
+      </div>
+    </div>
+  </MF>
+  `;
+};
+
+function Router() {
+  function routeChange() {
+    const container = document.getElementById("root");
+    container.innerHTML = "";
+    if (
+      window.location.hash !== "#/waiting" &&
+      window.location.hash !== "#/gamestart" &&
+      window.location.hash !== "#/gameover"
+    ) {
+      MiniFramework.render(Start, container);
+    } else if (window.location.hash === "#/waiting") {
+      MiniFramework.render(Waiting, container);
+    } else if (window.location.hash === "#/gamestart") {
+      MiniFramework.render(GameStart, container);
     }
+
+    // Set focus on the input textfield when the page is loaded
+    window.onload = () => {
+      const input = document.getElementById("nameplayer");
+      if (input !== null) {
+        input.focus();
+      }
+    };
+  }
+  window.onhashchange = routeChange;
+  routeChange();
+}
+
+function openChat() {
+  const chatDiv = document.createElement("div");
+  chatDiv.id = "player-chat";
+  const form = document.createElement("form")
+  const input = document.createElement("input")
+  const button = document.createElement("button")
+  form.id = "form"
+  button.innerText = "Send"
+  input.id = "input"
+  form.appendChild(input)
+  form.appendChild(button)
+  chatDiv.appendChild(form)
+  const chatMessages = document.createElement('div')
+  chatMessages.id = "chat-messages"
+  chatMessages.className = "chat-messages"
+  chatDiv.appendChild(chatMessages)
+  document.getElementById("core-part").appendChild(chatDiv);
+  console.log(document.getElementById("player-chat"))
+  console.log("openChat")
+  const nickname = localStorage.getItem("nickname");
+  console.log(nickname)
+
+  var socket = new WebSocket("ws://localhost:8080/ws");
+
+  socket.onopen = function () {
+    console.log("Connected!");
+
+    var payload = JSON.stringify({
+      type: "nickname",
+      nickname: nickname,
+    });
+
+    socket.send(payload);
   };
 
-  function openChat() {
-    const chatDiv = document.createElement("div");
-    chatDiv.id = "chat2";
-    document.getElementById("chat").appendChild(chatDiv);
+  socket.onerror = function (error) {
+    console.log("WebSocket error: " + error);
+  };
 
-    const nickname = localStorage.getItem("nickname")
+  socket.onmessage = function (event) {
+    var msg = JSON.parse(event.data);
+    console.log(msg);
+    var node = document.createElement("div");
+    var textnode = document.createTextNode(msg.nickname + ": " + msg.message);
+    node.appendChild(textnode);
+    document.getElementById("chat-messages").appendChild(node);
+  };
 
-    var socket = new WebSocket("ws://localhost:8080/ws");
+  document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-    socket.onopen = function () {
-      console.log("Connected!");
+    var input = document.getElementById("input");
+    var message = input.value;
+    input.value = "";
 
-      var payload = JSON.stringify({
-        type: "nickname",
-        nickname: nickname
-      })
-
-      socket.send(payload)
+    var msg = {
+      Type: "message",
+      Message: message,
     };
 
-    socket.onerror = function (error) {
-      console.log("WebSocket error: " + error);
-    };
-
-    socket.onmessage = function (event) {
-      var msg = JSON.parse(event.data);
-      console.log(msg)
-      var node = document.createElement("div");
-      var textnode = document.createTextNode(msg.nickname + ": " + msg.message);
-      node.appendChild(textnode);
-      document.getElementById("chat").appendChild(node);
-    };
-
-    document.getElementById("form").addEventListener("submit", function (e) {
-      e.preventDefault();
-
-      var input = document.getElementById("input");
-      var message = input.value;
-      input.value = "";
-
-      var msg = {
-        Type: "message",
-        Message: message,
-      };
-
-      socket.send(JSON.stringify(msg));
-    });
-    window.onhashchange = routeChange;
-    routeChange(); // Call routeChange to handle initial page load
-  }
-    }
+    socket.send(JSON.stringify(msg));
+  }) // Call routeChange to handle initial page load
+}
 
 Router();
